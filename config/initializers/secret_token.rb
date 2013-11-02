39397fbc5dba23f381e20f418bdbca9a31abe080
renatodex <renatodex@gmail.com>
2013-11-02 12:48:46 -0200

    Initialized rep for rakaton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RakatonRailsJasmine0211::Application.config.secret_token = '1e7548a757165bc3cf444ba6cd0b680bea0337961cc57b07e2fbd01858e81b9ce4a163c68dec816369f3e490bc71498bff818d7b899fa4333f49fef5e7949a38'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+RakatonRailsJasmine0211::Application.config.session_store :cookie_store, key: '_rakaton-rails-jasmine-0211_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# RakatonRailsJasmine0211::Application.config.session_store :active_record_store
